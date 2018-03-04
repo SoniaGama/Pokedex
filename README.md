516ac1eab8841c3588615719f3a36423d6d5e445
SoniGama <abinyuuu@gmail.com>
2018-03-04 02:06:46 -0600

    cambios js

--- README.md
+++ README.md
@@ -7,3 +7,28 @@
 - V4 Copia exacta en datos al mock up propuesto
 ###Extras:
 - Usar fetch
+
+guardar el endpoint como data en cada elemento
+evento click:
+despliega modal
+extrae informacion
+pinta la informacion
+
+modal dinamico- modal que cambie contenido
+
+contenido del modal:
+
+nombre pokemon --titulo
+
+frase pokemon --parrafo
+
+altura-peso-categoria-hanilidad-sexo 
+o
+ccapture rate, habbitat, color-- card o jumbotron
+
+tipo-bicho-veneno-title y boton? o info
+debilidad-fuego-volador-etc--title y boton? o info
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -3,60 +3,68 @@ const $searchInput = $('#search-input');
 const $containerPokemon = $('#container-pokemon');
 let $searchInputText;
 
-// crear funciion de loadpage
-
-function loadPage() {
-    $containerPokemon.html('');
-    // let dataPokemon = pokemonData(element);
-    // paintPokemon(dataPokemon, $containerPokemon);
-    getPokemons();
+function dataModal (event) { 
+    e.preventDefault();    
+    let endpointModal = event.target;
+    console.log(endpointModal);    
 }
 
-function getPokemons() {
+const paintPokemon = (pokeInfoObj) => { //esta funcion debe recibir la función de data como variable
+    let output = ``;
+    pokeInfoObj.forEach(element => {
+        let pokeName = element.name;
+        let apiPokeUrl = element.url;
+        let pokeId = element.id;
+        output += `    
+                <div class="card-deck float-left mt-3 mr-1 ml-1">
+                    <div class="card border">
+                        <button data-url="${apiPokeUrl}" type="button" class="btn modal-button" data-toggle="modal" data-target="#info-pokemon">
+                            <img class="card-img-top" src="https://dummyimage.com/200x200" alt="Card image cap">
+                        </button>                        
+                        <div class="card-body">
+                            <h5 class="card-title">${pokeName}</h5>
+                            <a href="${apiPokeUrl}" class="card-link" target="_blank">link</a>                        
+                        </div>
+                    </div>                   
+                </div>
+                `
+    });
+    $containerPokemon.html(output);
+}
+
+const failPoke = () => {
+    console.log('ERROR!');
+}
+
+const pokemonData = (element) => {
+    const pokemonsInfo = element.pokemon_entries;
+    
+    let pokeInfoObj = pokemonsInfo.map(element => {      
+       
+            const pokeId = element.entry_number;
+            const pokemons = element.pokemon_species;
+          
+            const pokeObj = {
+                name: pokemons.name,
+                url: pokemons.url           
+            }        
+            
+        return pokeObj;
+    });
+    paintPokemon(pokeInfoObj)
+}
+
+const getPokemons = () => {
     $.ajax({
         url: 'https://pokeapi.co/api/v2/pokedex/1/'
     }).done(pokemonData).fail(failPoke);
 }
 
-// revisar la funcion de data, crar un obeto y guardar en una variable
-const pokemonData = (element) => {
-    const pokemonsInfo = element.pokemon_entries;
+function loadPage () {
+    $containerPokemon.html('');
+    getPokemons();    
+    $('.modal-button').click(dataModal);
+};
 
-    let pokeInfoObj = pokemonsInfo.map((element, index) => {
-        const pokemons = element.pokemon_species;
-
-        const pokeObj = {
-            name: pokemons.name,
-            url: pokemons.url
-        }
-        return pokeObj;
-    });
-    paintPokemon(pokeInfoObj, $containerPokemon)
-}
-
-function paintPokemon(pokeInfoObj, $containerPokemon) { //esta funcion debe recibir la función de data como variable
-    let output = ``;   
-
-    pokeInfoObj.forEach(element => {
-        let pokeName = element.name;
-        let apiPokeUrl = element.url;
-        output += `
-         <section class="row">
-             <div class="card" style="width: 18rem;">
-                <img class = "card-img-top" src="https://dummyimage.com/200x200" alt="Pokemon">
-                <div class="card-body">
-                   <h5 class="card-title">${pokeName}</h5>
-                   <a href="${apiPokeUrl}" class="card-link" target="_blank">Pokemon link</a>
-                </div>
-             </div>
-         </section>
-         `
-    });
-    $containerPokemon.html(output);
-}
-
-function failPoke() {
-    console.log('ERRORRR!!!!!!!');
-}
 
 $(document).ready(loadPage);
